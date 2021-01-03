5cce9430386465c8f595c505e35159c567269427
klevchenko <sigmund313@gmail.com>
2021-01-03 16:19:51 +0200

    init commit

---
+++ StarNaviTestTask/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for StarNaviTestTask project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'StarNaviTestTask.settings')
+
+application = get_asgi_application()
