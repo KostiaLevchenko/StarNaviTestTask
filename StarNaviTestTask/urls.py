85e435ddc653e762b1dd397ad9f81712d78508e6
klevchenko <sigmund313@gmail.com>
2021-01-05 21:43:52 +0200

    added routing for post requests

--- StarNaviTestTask/urls.py
+++ StarNaviTestTask/urls.py
@@ -8,15 +8,17 @@ from rest_framework import routers
 
 from knox.views import LogoutView
 
+from apps.post import views as post_views
 from apps.user_profile import views as user_profile_views
 
 router = routers.DefaultRouter()
+router.register(r'post', post_views.PostViewSet, basename='post')
 router.register(r'auth-confirm', user_profile_views.AuthConfirm, basename='auth-confirm')
 
 
 urlpatterns = [
     re_path(r'^api/', include(router.urls)),
-    re_path('admin/', admin.site.urls),
+    re_path(r'^admin/', admin.site.urls),
     re_path(r'^api/auth/sign-up/', user_profile_views.RegistrationView.as_view(), name='sign-up'),
     re_path(r'^api/auth/sign-in/', user_profile_views.LoginView.as_view(), name='sign-in'),
     re_path(r'^api/auth/logout/', LogoutView.as_view(), name='auth-logout'),
