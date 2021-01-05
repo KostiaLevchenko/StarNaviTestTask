3348b4a58de7f3effd0e0919aa41dee34283dc72
klevchenko <sigmund313@gmail.com>
2021-01-05 21:45:11 +0200

    implemented Post serializer

---
+++ apps/post/serializer.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from apps.post.models import Post
+
+
+class PostSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Post
+        fields = ('title', 'body', 'author', 'creation_date')
+
+    def create(self, validated_data):
+        post = Post.objects.create(**validated_data)
+        return post
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.body = validated_data.get('body', instance.body)
+        instance.creation_date = validated_data.get('creation_date', instance.creation_date)
+        instance.save()
+        return instance
