487ad0d3bded6a9881aa1911af64ea3de6210d3f
klevchenko <sigmund313@gmail.com>
2021-01-07 02:27:52 +0200

    code style refactoring

--- apps/post/views.py
+++ apps/post/views.py
@@ -1,13 +1,7 @@
-from django.db.models import Count
-from django.db.models.functions import TruncDay
-
-from datetime import datetime
-
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from apps.post.models import Like
 from apps.post.services.post_service import PostService
 from apps.user_profile.services.user_profile_service import UserProfileService
 
@@ -17,7 +11,7 @@ class PostViewSet(viewsets.ModelViewSet):
     def create_post(self, request, *args, **kwargs):
         try:
             profile = UserProfileService.get(profile_data={'email': request.user})
-            post = PostService.create(post_data=request.data, profile=profile)
+            post = PostService.create(post_data=request.data.copy(), profile=profile)
             return Response(post, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
@@ -47,21 +41,3 @@ class PostViewSet(viewsets.ModelViewSet):
             return Response(like, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-    @action(detail=False, methods=['GET'])
-    def analytics(self, request, *args, **kwargs):
-        try:
-            date_from = self.request.query_params.get('date_from')
-            date_to = self.request.query_params.get('date_to')
-            likes = Like.objects.\
-                filter(
-                    like_date__range=(datetime.strptime(date_from, '%Y-%m-%d'), datetime.strptime(date_to, '%Y-%m-%d'))
-                )\
-                .annotate(day=TruncDay('like_date'))\
-                .values('day')\
-                .annotate(likes_per_day=Count('id'))\
-                .order_by()
-            return Response(list(likes), status=status.HTTP_200_OK)
-        except Exception as e:
-            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
